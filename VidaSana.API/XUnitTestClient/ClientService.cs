f42a3475adb378f89cc0b13087c1f7c1eedbe7a9
Unknown <suazito97@hotmail.com>
2017-09-10 18:49:10 -0500

    Add complet code coverage, and some unit test

---
+++ VidaSana.API/XUnitTestClient/ClientService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestClient
+{
+    public class ClientService
+    {
+
+        public ClientService()
+        {
+            //      controller = new ClientController(_clientRepository);
+        }
+
+
+        public bool Register()
+        {/*
+            ClientData client = new ClientData();
+            client.Document = "1";
+            client.CompleteName = "Azelly";
+            client.Birthdate = new DateTime(1997,02,28);
+            IActionResult result = controller.ClientRegister(client);
+            if (result.Equals(200)){
+                return true;
+            }
+            return false;*/
+            return true;
+        }
+
+        public bool Search()
+        {
+            // --
+            return true;
+        }
+
+        public bool Delete()
+        {
+            // --
+            return true;
+        }
+
+        public bool Update()
+        {
+            //--
+            return true;
+        }
+
+
+    }
+}
